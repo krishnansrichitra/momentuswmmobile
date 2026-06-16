dd7e3bdb55c12938561744e0b0d36a417d59919f
krishnansrichitra <krishnanhd25@gmail.com>
2026-06-16 15:55:08 +0530

    Recieinv SCR

--- lib/screens/common/builders/widget_builder.dart
+++ lib/screens/common/builders/widget_builder.dart
@@ -2,7 +2,8 @@ import '../models/field_dto.dart';
 import 'package:flutter/material.dart';
 
 
-  Widget buildField(FieldDto field,
+  Widget buildField(  BuildContext context,
+      FieldDto field,
       Map<String, TextEditingController> controllers) {
     if (field.type == "data_type_str") {
       controllers.putIfAbsent(
@@ -10,28 +11,90 @@ import 'package:flutter/material.dart';
             () => TextEditingController(),
       );
 
-      return TextField(
+      return TextFormField(
         controller: controllers[field.accessor],
         decoration: InputDecoration(
-          labelText: field.label,
+          labelText: field.mandatory ?? false
+              ? "${field.label} *"
+              : field.label,
         ),
+        validator: (value) {
+          if ((field.mandatory ?? false) &&
+              (value == null || value.trim().isEmpty)) {
+            return "${field.label} is required";
+          }
+          return null;
+        },
       );
     }
 
-    if (field.type == "number") {
+    if (field.type == "data_type_long") {
       controllers.putIfAbsent(
         field.accessor,
             () => TextEditingController(),
       );
 
-      return TextField(
+      return TextFormField(
         controller: controllers[field.accessor],
         keyboardType: TextInputType.number,
         decoration: InputDecoration(
-          labelText: field.label,
+          labelText: field.mandatory ?? false
+              ? "${field.label} *"
+              : field.label,
         ),
+        validator: (value) {
+          if ((field.mandatory ?? false) &&
+              (value == null || value.trim().isEmpty)) {
+            return "${field.label} is required";
+          }
+          return null;
+        },
       );
     }
 
+
+    if (field.type == "data_type_date") {
+      controllers.putIfAbsent(
+        field.accessor,
+            () => TextEditingController(),
+      );
+
+      return TextFormField(
+        controller: controllers[field.accessor],
+        readOnly: true,
+        decoration: InputDecoration(
+          labelText: (field.mandatory ?? false)
+              ? "${field.label} *"
+              : field.label,
+          suffixIcon: const Icon(Icons.calendar_today),
+        ),
+        onTap: () async {
+          DateTime? pickedDate = await showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2000),
+            lastDate: DateTime(2100),
+          );
+
+          if (pickedDate != null) {
+            controllers[field.accessor]!.text =
+            "${pickedDate.year}-"
+                "${pickedDate.month.toString().padLeft(2, '0')}-"
+                "${pickedDate.day.toString().padLeft(2, '0')}";
+          }
+        },
+        validator: (_) {
+          final text =
+              controllers[field.accessor]?.text ?? "";
+
+          if ((field.mandatory ?? false) &&
+              text.trim().isEmpty) {
+            return "${field.label} is required";
+          }
+
+          return null;
+        },
+      );
+    }
     return const SizedBox.shrink();
   }
--- lib/screens/receiving/controller/receiving_controller.dart
+++ lib/screens/receiving/controller/receiving_controller.dart
@@ -8,9 +8,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import '../models/receiving_screen_dto.dart';
 
-
-
-
 class ReceivingScreen extends StatefulWidget {
   final ScreenDto screenDto;
   final int screenNo;
@@ -29,48 +26,53 @@ class ReceivingScreen extends StatefulWidget {
 
 class _ReceivingScreenState extends State<ReceivingScreen> {
   final Map<String, TextEditingController> controllers = {};
+  final _formKey = GlobalKey<FormState>();
 
-  Future<void> loadNextScreen(Map<String, dynamic> request) async
-  {
+  Future<void> loadNextScreen(Map<String, dynamic> request) async {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString("jwt");
-    final uri = Uri.parse(
-        "${AppConfig.apiBaseUrl}/wms/api/rcvcontroller/submitreceiving"
-    ).replace(
-      queryParameters: {
-        "template": widget.template,
-        "screenNo":widget.screenNo.toString(),
-        "action":"next"
-      },
-    );
+    final uri =
+        Uri.parse(
+          "${AppConfig.apiBaseUrl}/wms/api/rcvcontroller/submitreceiving",
+        ).replace(
+          queryParameters: {
+            "template": widget.template,
+            "screenNo": widget.screenNo.toString(),
+            "action": "next",
+          },
+        );
     final response = await http.post(
       uri,
       headers: {
         "Authorization": "Bearer $token",
         "Content-Type": "application/json",
       },
-      body: jsonEncode(request)
+      body: jsonEncode(request),
     );
 
     if (response.statusCode == 200) {
-       ReceivingScreenDto result =ReceivingScreenDto.fromJson(jsonDecode(response.body));
-       print (result);
-       Navigator.push(
-         context,
-         MaterialPageRoute(
-           builder: (context) => ReceivingScreen(
-             screenDto: result.mobileScreenDTO,
-             screenNo: result.screenNo,
-             template: result.template,
-           ),
-         ),
-       );
+      ReceivingScreenDto result = ReceivingScreenDto.fromJson(
+        jsonDecode(response.body),
+      );
+      print(result);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ReceivingScreen(
+            screenDto: result.mobileScreenDTO,
+            screenNo: result.screenNo,
+            template: result.template,
+          ),
+        ),
+      );
     }
-
   }
 
   void submit() {
     Map<String, dynamic> request = {};
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
 
     for (var field in widget.screenDto.fields ?? []) {
       request[field.accessor] = controllers[field.accessor]?.text;
@@ -92,25 +94,28 @@ class _ReceivingScreenState extends State<ReceivingScreen> {
       appBar: AppBar(title: const Text("Receiving")),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ...(widget.screenDto.fields ?? []).map(
-              (field) => buildField(field, controllers),
-            ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              ...(widget.screenDto.fields ?? []).map(
+                (field) => buildField(context,field, controllers),
+              ),
 
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                if (widget.screenNo == 1)
-                  ElevatedButton(
-                    onPressed: previous,
-                    child: const Text("Previous"),
-                  ),
-                const SizedBox(width: 20),
-                ElevatedButton(onPressed: submit, child: const Text("Next")),
-              ],
-            ),
-          ],
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  if (widget.screenNo == 1)
+                    ElevatedButton(
+                      onPressed: previous,
+                      child: const Text("Previous"),
+                    ),
+                  const SizedBox(width: 20),
+                  ElevatedButton(onPressed: submit, child: const Text("Next")),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
